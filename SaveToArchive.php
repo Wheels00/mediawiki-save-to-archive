4100a9c800b187ec3876d5537bb37487829f7e09
Mark Brown <mark@clickability.com.au>
2019-07-02 10:14:00 +1000

    fix

--- SaveToArchive.php
+++ SaveToArchive.php
@@ -1,22 +1,22 @@
 <?php
 
+
 namespace SaveToArchive;
 
 class SaveToArchive
 {
   public static function onLinkerMakeExternalLink($url, $text, &$link, array &$attribs, $linktype)
   {
-
+      
     // create a new cURL resource
     $ch = curl_init();
 
     // set URL and other appropriate options
     curl_setopt($ch, CURLOPT_URL, "https://web.archive.org/save/".$url);
-    curl_setopt($curl, CURLOPT_URL, $this->url);
-    curl_setopt($curl, CURLOPT_FILETIME, true);
-    curl_setopt($curl, CURLOPT_NOBODY, true);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_FILETIME, true);
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, true);
 
     // grab URL and pass it to the browser
     curl_exec($ch);
